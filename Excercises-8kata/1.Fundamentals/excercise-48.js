3de5539896faa1ec71d5d5401b7565209541a5bb
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-12-31 16:19:04 +0100

    Grasshopper - Summation - solution

---
+++ Excercises-8kata/1.Fundamentals/excercise-48.js
@@ -0,0 +1,15 @@
+// Grasshopper - Summation
+
+var summation = function (num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+    return sum
+}var summation = function (num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+    return sum
+}
